ee1c9e046b6b431c1b300b890257ec5fe18298f8
harbinxxh <harbinxxh@gmail.com>
2018-08-04 14:21:08 +0800

    添加：BlackHole 功能。模型绕黑洞旋转并被吸入黑洞中。

---
+++ Source/FPSGame/Private/Challenges/FPSBlackHole.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSBlackHole.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+AFPSBlackHole::AFPSBlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+
+	InnerSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("InnerSphereComp"));
+	InnerSphereComponent->SetSphereRadius(100);
+	InnerSphereComponent->SetupAttachment(MeshComp);
+
+	// Bind to Event
+	InnerSphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AFPSBlackHole::OverlapInnerSphere);
+
+	OuterSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("OuterSphereComp"));
+	OuterSphereComponent->SetSphereRadius(3000);
+	OuterSphereComponent->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSBlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AFPSBlackHole::OverlapInnerSphere(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		OtherActor->Destroy();
+	}
+}
+
+// Called every frame
+void AFPSBlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Find all overlapping components that can collide and may be physically simulating.
+	TArray<UPrimitiveComponent*> OverlappingComp;
+	OuterSphereComponent->GetOverlappingComponents(OverlappingComp);
+
+	for (int32 i = 0; i < OverlappingComp.Num(); i++)
+	{
+		UPrimitiveComponent* PrimComp = OverlappingComp[i];
+		if (PrimComp && PrimComp->IsSimulatingPhysics())
+		{
+			// the component we are looking for! It needs to be simulating in other to apply forces.
+
+			const float SphereRadius = OuterSphereComponent->GetScaledSphereRadius();
+			const float ForceStrength = -2000; // Negative value to make it pull towards the origin instead of pushing away
+
+			PrimComp->AddRadialForce(GetActorLocation(), SphereRadius, ForceStrength, ERadialImpulseFalloff::RIF_Constant, true);
+		}
+	}
+
+}
+
